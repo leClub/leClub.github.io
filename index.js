0273d20e0721a3449901c75a86f21067a38b7b37
Clément Renaud <clement.renaud@gmail.com>
2015-01-03 16:43:24 +0100

    sass support

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var Metalsmith = require('metalsmith'),
-    markdown   = require('metalsmith-markdown'),
-    collections = require('metalsmith-collections'),
-    permalinks  = require('metalsmith-permalinks'),
-    templates  = require('metalsmith-templates'),
-    handlebars = require('handlebars'),
-    fs = require('fs');
+markdown   = require('metalsmith-markdown'),
+collections = require('metalsmith-collections'),
+permalinks  = require('metalsmith-permalinks'),
+templates  = require('metalsmith-templates'),
+sass = require('metalsmith-sass'),
+handlebars = require('handlebars'),
+highlight  = require('highlight.js'),
+fs = require('fs');
 
 handlebars.registerPartial('header', fs.readFileSync(__dirname + '/templates/partials/header.html').toString());
 handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/partials/footer.html').toString());
@@ -24,11 +26,27 @@ Metalsmith(__dirname)
     .use(permalinks({
         pattern: ':collection/:title'
     }))
-    .use(markdown())
+    .use(sass({
+        outputStyle: 'compact'
+    }))
+    .use(markdown({
+        gfm: true,
+        tables: true,
+        highlight: function (code, lang) {
+            if (!lang) {
+                return code;
+            }
+            try {
+                return highlight.highlight(lang, code).value;
+            } catch (e) {
+                return code;
+            }
+        }
+    }))
     .use(templates('handlebars'))
     .destination('./build')
     .build(function (err) {
         if (err) {
           throw err;
       }
-    })
+  })
